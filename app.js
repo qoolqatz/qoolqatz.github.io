01f102bfa71b0f58ec4ebaa1121899575d487d6b
uniXros <141985136+unixros@users.noreply.github.com>
2024-06-22 15:37:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,19 @@
 let count = 0;
 
 function checkLotto() {
-  count++;
   let lotto_number = document.getElementById("lotto_number").value;
+
+  // ตรวจสอบว่าค่าเป็นตัวเลข 2 หลักหรือไม่
+  if (!/^\d{1,2}$/.test(lotto_number)) {
+    alert("ระบุตัวเลขได้เท่านั่น โดยเป็น 1 หรือ 2 หลัก");
+    return;
+  }
+
+  count++;
   let random_number = Math.floor(Math.random() * 100);
 
-  console.log(lotto_number)
-  console.log(random_number)
+  console.log(lotto_number);
+  console.log(random_number);
 
   document.getElementById("lotto").innerText = lotto_number;
   document.getElementById("random").innerText = random_number;
